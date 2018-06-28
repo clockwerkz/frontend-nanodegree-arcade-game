7aaa381cdd4566f25b824261f962510e46732e42
Carlos Fins <clockwerkz@gmail.com>
2018-06-27 19:04:38 -0700

    adds heart and rock to resource list in engine.js.

--- js/engine.js
+++ js/engine.js
@@ -194,6 +194,8 @@ var Engine = (function(global) {
         'images/Gem Blue.png',
         'images/Gem Green.png',
         'images/Gem Orange.png',
+        'images/Rock.png',
+        'images/Heart.png'
     ]);
     Resources.onReady(init);
 
